962b5aaf1080c54ecc5a99af2c92572a7e94f2c3
hieupd21 <hieupd21@gmail.com>
2020-05-31 08:07:09 +0700

    edit bubble

--- html/10-20200525/bubble/script.js
+++ html/10-20200525/bubble/script.js
@@ -1,7 +1,9 @@
-document.addEventListener("DOMContentLoaded",function(){
-    var btn = document.getElementById('btn');
-    var img = document.getElementById('img');
-    btn.onclick = function() {
-        img.classList.toggle('on');
+function turn() {
+    var anhElement = document.getElementById('anh');
+    console.log(anhElement.src);
+    if (anhElement.src == 'http://127.0.0.1:5500/html/10-20200525/bubble/off.gif') {
+        anhElement.src = 'on.gif';
+    } else {
+        anhElement.src = 'off.gif';
     }
-},false)
+}
